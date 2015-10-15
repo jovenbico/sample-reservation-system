f2629fcca0612d5ff7d596be35a6e6ac7bbb96df
Joven Frankie Bico <jovenbico@gmail.com>
2015-10-15 18:27:33 +0800

    $

---
+++ src/main/java/com/bicjo/resys/domain/User.java
@@ -0,0 +1,12 @@
+package com.bicjo.resys.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3279544047957545986L;
+
+}
