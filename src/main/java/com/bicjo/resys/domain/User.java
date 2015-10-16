06e43c8c00a43ba9cfc9498330cb303f8e795739
Joven Frankie Bico <jovenbico@gmail.com>
2015-10-16 19:24:25 +0800

    configure Spring and Hibernate

--- src/main/java/com/bicjo/resys/domain/User.java
+++ src/main/java/com/bicjo/resys/domain/User.java
@@ -2,11 +2,34 @@ package com.bicjo.resys.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "users")
 public class User implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 3279544047957545986L;
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String username;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
 }
