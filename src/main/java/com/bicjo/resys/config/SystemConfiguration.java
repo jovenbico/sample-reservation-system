c14f19b7c037052e6f1f51aabf515e778b524364
bicjo <jovenbico@gmail.com>
2015-10-19 20:21:54 +0800

    enable resource filtering

--- src/main/java/com/bicjo/resys/config/SystemConfiguration.java
+++ src/main/java/com/bicjo/resys/config/SystemConfiguration.java
@@ -5,8 +5,11 @@ import java.util.Properties;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -14,17 +17,20 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
+@PropertySource(value = { "classpath:config.properties" })
 public class SystemConfiguration {
 
-	// TODO enable resource filtering
-	// http://jlorenzen.blogspot.com/2013/06/resource-filtering-with-gradle.html
+	@Autowired
+	private Environment env;
+
 	@Bean
 	public DataSource dataSource() {
 		BasicDataSource dataSource = new BasicDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost/reservation_system?createDatabaseIfNotExist=true");
-		dataSource.setUsername("@db.username@");
-		dataSource.setPassword("@db.password@");
+		dataSource
+				.setUrl("jdbc:mysql://localhost/reservation_system?createDatabaseIfNotExist=true");
+		dataSource.setUsername(this.env.getProperty("db.username"));
+		dataSource.setPassword(this.env.getProperty("db.password"));
 
 		return dataSource;
 	}
@@ -34,12 +40,13 @@ public class SystemConfiguration {
 		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
 
 		Properties hibernateProperties = new Properties();
-		hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+		hibernateProperties.put("hibernate.dialect",
+				"org.hibernate.dialect.MySQLDialect");
 		hibernateProperties.put("hibernate.hbm2ddl.auto", "update");
 		hibernateProperties.put("hibernate.show_sql", "true");
 		hibernateProperties.put("hibernate.format_sql", "true");
 
-		sessionFactory.setDataSource(dataSource());
+		sessionFactory.setDataSource(this.dataSource());
 		sessionFactory.setHibernateProperties(hibernateProperties);
 		sessionFactory.setPackagesToScan("com.bicjo.resys.domain");
 
@@ -49,7 +56,7 @@ public class SystemConfiguration {
 	@Bean
 	public PlatformTransactionManager transactionManager() {
 		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-		transactionManager.setSessionFactory(sessionFactory().getObject());
+		transactionManager.setSessionFactory(this.sessionFactory().getObject());
 
 		return transactionManager;
 	}
